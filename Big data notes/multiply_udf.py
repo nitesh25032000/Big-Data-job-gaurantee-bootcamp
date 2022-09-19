98bb39011df1758a5d9177d29d2a908a5a33739d
Nitesh S <106883023+nitesh25032000@users.noreply.github.com>
2022-09-19 21:47:42 +0530

    Delete Big data notes directory

--- Big data notes/multiply_udf.py
+++
@@ -1,7 +0,0 @@
-import sys
-                                                                                                                                              
-for line in sys.stdin:
-        line = line.strip("\n\r")
-        quantity = int(line)
-        quantity = quantity * 10
-        print(str(quantity))
